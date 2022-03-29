b5590b204b177b87337e111603881c9517d9fcff
Arielli Andrea <76228047+SwitchArio@users.noreply.github.com>
2022-03-29 15:05:01 +0200

    Update main.cpp

--- matrici03/main.cpp
+++ matrici03/main.cpp
@@ -1,3 +1,4 @@
+// ANDREA ARIELLI - CALCOLO MATRICE INVERSA
 #include <iostream>
 #include <math.h>
 
@@ -5,102 +6,106 @@ using namespace std;
 
 const int MAX = 10;
 
-int matriceInversa(int[][MAX], int);
-int matriceTrasposta(int[][MAX], int);
-int stampaMatrice(int[][MAX], int);
-int determinante(int[][MAX], int);
+void matriceInversa(int[MAX][MAX], int);
+void stampaMatrice(int[MAX][MAX], int);
+int determinante(int[MAX][MAX], int);
 
 int main()
 {
-    int ordine = 3;
-    int M[MAX][MAX]{
-    {1, 2, 3},
-    {4, 5, 6},
-    {7, 8, 9}};
-    int N = matriceInversa(M, ordine);
-    stampaMatrice(M, ordine);
-    return 0;
+    int ordine = 3;
+    int M[MAX][MAX]{ // matrice con determinante 0
+    {1, 2, 3},
+    {4, 5, 6},
+    {7, 8, 9}};
+
+    int M2[MAX][MAX]{ // matrice con determinante 0
+    {1, 2, 3, 4},
+    {5, 6, 7, 8},
+    {9, 10, 11, 12},
+    {13, 14, 15, 16}};
+
+    /*if (determinante(M, ordine) == 0){
+        cout << "il determinante e' 0, la matrice non si puo' invertire";
+        return 0;
+    }*/
+
+    stampaMatrice(M, ordine);
+    matriceInversa(M, ordine);
+
+    return 0;
 }
 
-int matriceInversa(int A[MAX][MAX], int ordine){
-    int B[MAX][MAX], SM[MAX][MAX];
+void matriceInversa(int A[MAX][MAX], int ordine){
+    int B[MAX][MAX], SM[MAX][MAX];
+    int ordineSM = ordine-1;
 
+    // scorro la riga e colonna da togliere
+    for(int rTolta = 0; rTolta < ordine; rTolta++)
+        for(int cTolta = 0; cTolta < ordine; cTolta++)
+        {
+            int rSM = 0, cSM = 0;
 
+            // per ogni cella della matrice
+            // calcolo il determinante della sottomatrice
+            for(int r = 0; r < ordine; r++)
+                for(int c = 0; c < ordine; c++)
+                    if(r != rTolta && c != cTolta) // se la cella corrente non è una di quelle tolte
+                    {
+                        SM[rSM][cSM] = A[r][c];
 
-    // scorro la riga e colonna da togliere
-    for(int rTolta = 0; rTolta < ordine; rTolta++)
-        for(int cTolta = 0; cTolta < ordine; cTolta++)
-        {
-            int rSM = 0, cSM = 0;
-            // prendo la matrice togliendo la riga e colonna corrente
-            for(int r = 0; r < ordine; r++)
-                for(int c = 0; c < ordine; c++)
-                {
-                    if(r != rTolta && c != cTolta){
-                        SM[rSM][cSM] = A[r][c];
-                        if(cSM == 0) cSM++;
-                        else if(cSM == 1) {rSM++;cSM = 0;}
-                        if(cSM == 1 && rSM == 2) {B[rTolta][cTolta] = determinante(SM, ordine-1);}
-                    }
-                }
-/*
-|-, -, -|
-|4, -, 6|
-|7, -, 9|
+                        if(cSM != ordineSM-1) cSM++;
+                        else if(cSM == ordineSM-1) { rSM++; cSM = 0; }
+                        if(rSM == ordineSM)
+                            B[rTolta][cTolta] = determinante(SM, ordine-1) * pow(-1, rTolta+cTolta);
+                    }
+        }
 
-|, |
-|, |
-*/
-        }
-    int matriceFinale = matriceTrasposta(B, ordine);
-    stampaMatrice(B, ordine);
-    return matriceFinale;
+    // faccio la trasposta della matrice
+    int matriceFinale[MAX][MAX];
+    for(int r = 0; r < ordine; r++)
+        for(int c = 0; c < ordine; c++)
+            matriceFinale[r][c] = B[c][r];
+
+    stampaMatrice(matriceFinale, ordine);
+
 }
 
-int matriceTrasposta(int A[MAX][MAX], int ordine){
-    int B[MAX][MAX];
+void stampaMatrice(int A[MAX][MAX], int len){
+    for(int r = 0; r < len; r++){
+        cout << "| ";
+        for(int c = 0; c < len; c++)
+        {
+            if(A[r][c] >= 0) cout << " ";
 
-    for(int r = 0; r < ordine; r++)
-        for(int c = 0; c < ordine; c++)
-            B[r][c] = A[c][r];
-    stampaMatrice(B, ordine);
-    return B;
-}
+            cout << A[r][c];
 
-int stampaMatrice(int A[MAX][MAX], int len){
-    for(int r = 0; r < len; r++){
-        cout << "| ";
-        for(int c = 0; c < len; c++)
-        {
-            if(A[r][c] >= 0) cout << " ";
-
-            cout << A[r][c];
-
-            if(c!=len-1) cout << ", ";
-        }
-        cout << " |\n";
-    }
+            if(c!=len-1) cout << ", ";
+            if(abs(A[r][c]) < 10) cout << " ";
+        }
+        cout << " |\n";
+    }
+    cout << "\n";
 }
 
 int determinante(int A[MAX][MAX], int ordine){
+    if(ordine == 1) return A[0][0];
+    else if(ordine == 2) return A[0][0] * A[1][1] - A[0][1] * A[1][0];
 
-    if(ordine == 1) return A[0][0];
-    else if(ordine == 2) return A[0][0] * A[1][1] - A[0][1] * A[1][0];
+    int tot = 0, SM[MAX][MAX];
 
-    int tot = 0, SM[MAX][MAX];
+    for(int riga = 0; riga < ordine; riga++){
 
-    for(int riga = 0; riga < ordine; riga++){
+        // riempimento SM
+        for(int rSM = 0; rSM < ordine-1; rSM++)
+            for(int cSM = 0; cSM < ordine-1; cSM++){
 
-        // riempimento SM
-        for(int rSM = 0; rSM < ordine-1; rSM++)
-            for(int cSM = 0; cSM < ordine-1; cSM++){
+            int r = rSM < riga ? r = rSM : rSM+1;
+            SM[rSM][cSM] = A[r][cSM+1];
 
-            int r = rSM < riga ? r = rSM : rSM+1;
-            SM[rSM][cSM] = A[r][cSM+1];
+        }
 
-        }
+        tot += A[riga][0] * pow(-1, riga) * determinante(SM, ordine-1);
+    }
 
-        tot += A[riga][0] * pow(-1, riga) * determinante(SM, ordine-1);
-    }
-    return tot;
+    return tot;
 }
